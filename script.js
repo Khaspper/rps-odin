084fb1f556c4cee51cebd9942587988a898fbd93
Markus <brandonnarciso24@gmail.com>
2023-07-13 18:42:09 -0700

    Fix Scoring system
    for some reason it kept the old score so now it resets the score

--- script.js
+++ script.js
@@ -21,31 +21,60 @@ function playRound(playerSelection = prompt("Rock, Paper or Scissors?"), compute
     if (playerSelection === 0) {
         switch (computerSelection) {
             case 0:
-                return "Tie Game!";
+                console.log("Tie Game!");
+                return 0;
             case 1:
-                return "You Lose! Paper beats Rock";
+                console.log("You Lose! Paper beats Rock");
+                return -1;
             default:
-                return "Tou Win! Rock beats Scissors";
+                console.log("Tou Won! Rock beats Scissors");
+                return 1;
         }
     }
     else if (playerSelection === 1) {
         switch (computerSelection) {
             case 0:
-                return "Tou Win! Paper beats Rock";
+                console.log("Tou Won! Paper beats Rock");
+                return 1;
             case 1:
-                return "Tie Game!";
+                console.log("Tie Game!");
+                return 0;
             default:
-                return "You Lose! Scissors beats Paper";
+                console.log("You Lose! Scissors beats Paper");
+                return -1;
         }
     }
     else {
         switch (computerSelection) {
             case 0:
-                return "Tou Lose! Rock beats Scissors";
+                console.log("Tou Lose! Rock beats Scissors");
+                return -1;
             case 1:
-                return "You Win! Scissors beats Paper";
+                console.log("You Won! Scissors beats Paper");
+                return 1;
             default:
-                return "Tie Game!";
+                console.log("Tie Game!");
         }
+    }
+}
+
+function game() {
+    let roundResult;
+    let playerScore = 0;
+    let cpuScore = 0;
+    for (let i = 0; i < 5; i++) {
+        roundResult = playRound("Rock");
+        if (roundResult === 1) {
+            playerScore++;
+            if (playerScore === 3)
+                return `You Won!\nYour Score:${playerScore}\nCPU Score:${Math.abs(cpuScore)}`;
+        }
+        else if (roundResult === -1) {
+            cpuScore++;
+            if (cpuScore === 3)
+                return `You Lost!\nYour Score:${playerScore}\nCPU Score:${Math.abs(cpuScore)}`;
+        }
+        else if (roundResult === 0)
+            i--;
     }
 }
