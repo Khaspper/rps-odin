34c76cd6d91db92406893f42c9c816d100015202
Markus <brandonnarciso24@gmail.com>
2023-07-13 17:49:49 -0700

    Add winning system
    This is to determine who won the battle and prints out if the user won
    or not

--- script.js
+++ script.js
@@ -2,3 +2,50 @@ function getComputerChoice() {
     let weapon = Math.floor(Math.random() * 3);
     return weapon;
 }
+
+function startGame(playerSelection = prompt("Rock, Paper or Scissors?"), computerSelection = getComputerChoice()) {
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            playerSelection = 0;
+            break;
+        case "paper":
+            playerSelection = 1;
+            break;
+        case "scissors":
+            playerSelection = 2;
+            break;
+        default:
+            break;
+    }
+
+    if (playerSelection === 0) {
+        switch (computerSelection) {
+            case 0:
+                return "Tie Game!";
+            case 1:
+                return "You Lose! Paper beats Rock";
+            default:
+                return "Tou Win! Rock beats Scissors";
+        }
+    }
+    else if (playerSelection === 1) {
+        switch (computerSelection) {
+            case 0:
+                return "Tou Win! Paper beats Rock";
+            case 1:
+                return "Tie Game!";
+            default:
+                return "You Lose! Scissors beats Paper";
+        }
+    }
+    else {
+        switch (computerSelection) {
+            case 0:
+                return "Tou Lose! Rock beats Scissors";
+            case 1:
+                return "You Win! Scissors beats Paper";
+            default:
+                return "Tie Game!";
+        }
+    }
+}
