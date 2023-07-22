87b8fff632bf36ba44a916fe0f3310247f8dc6be
Markus <brandonnarciso24@gmail.com>
2023-07-21 21:21:26 -0700

    Add who wins
    who ever wins the output states who one

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getComputerChoice() {
     return weapon;
 }
 
-function playRound(playerSelection, computerSelection = getComputerChoice()) {
+function playRound(playerSelection, computerSelection = getComputerChoice(), userScore, comScore) {
     const resultsDIV = document.getElementById("results");
     //! This is how you remove the child element from the parent and update it
     //! So it's a different result reach time
@@ -35,14 +35,14 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
                 outcome.textContent = "You Lose! Paper beats Rock";
                 return -1;
             default:
-                outcome.textContent = "Tou Won! Rock beats Scissors";
+                outcome.textContent = "You Won! Rock beats Scissors";
                 return 1;
         }
     }
     else if (playerSelection === 1) {
         switch (computerSelection) {
             case 0:
-                outcome.textContent = "Tou Won! Paper beats Rock";
+                outcome.textContent = "You Won! Paper beats Rock";
                 return 1;
             case 1:
                 outcome.textContent = "Tie Game";
@@ -55,7 +55,7 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     else {
         switch (computerSelection) {
             case 0:
-                outcome.textContent = "Tou Lose! Rock beats Scissors";
+                outcome.textContent = "You Lose! Rock beats Scissors";
                 return -1;
             case 1:
                 outcome.textContent = "You Won! Scissors beats Paper";
@@ -66,41 +66,67 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     }
 }
 
-// function game() {
-//     let roundResult;
-//     let playerScore = 0;
-//     let cpuScore = 0;
-//     for (let i = 0; i < 5; i++) {
-//         roundResult = playRound();
-//         if (roundResult === 1) {
-//             playerScore++;
-//             if (playerScore === 3)
-//                 return `You Won!\nYour Score:${playerScore}\nCPU Score:${Math.abs(cpuScore)}`;
-//         }
-//         else if (roundResult === -1) {
-//             cpuScore++;
-//             if (cpuScore === 3)
-//                 return `You Lost!\nYour Score:${playerScore}\nCPU Score:${Math.abs(cpuScore)}`;
-//         }
-//         else if (roundResult === 0)
-//             i--;
-//     }
-// }
+function updateScore(userScore, comScore) {
 
+}
 
+function playerWins() {
+    const resultsDIV = document.getElementById("results");
+    while (resultsDIV.firstChild) {
+        resultsDIV.removeChild(resultsDIV.firstChild);
+    }
+    const outcome = document.createElement("div");
+    outcome.textContent = "You WON!!!!";
+    resultsDIV.appendChild(outcome);
+}
+
+function cpuWins() {
+    const resultsDIV = document.getElementById("results");
+    while (resultsDIV.firstChild) {
+        resultsDIV.removeChild(resultsDIV.firstChild);
+    }
+    const outcome = document.createElement("div");
+    outcome.textContent = "You LOST!!!!";
+    resultsDIV.appendChild(outcome);
+}
+
+function game() {
+    let roundResult = 0;
+    let playerScore = 0;
+    let cpuScore = 0;
+
+    const rockButton = document.getElementById("rockButton");
+    const paperButton = document.getElementById("paperButton");
+    const scissorsButton = document.getElementById("scissorsButton");
+
+    rockButton.addEventListener("click", (event) => {
+        roundResult = playRound(playerSelection = "rock");
+        if(roundResult === 1) playerScore++;
+        else if(roundResult === -1) cpuScore++;
+        if(playerScore === 5) playerWins();
+        else if(cpuScore === 5) playerWins();
+    });
+    paperButton.addEventListener("click", (event) => {
+        roundResult = playRound(playerSelection = "paper");
+        console.log(roundResult);
+        if(roundResult === 1) playerScore++;
+        else if(roundResult === -1) cpuScore++;
+        if(playerScore === 5) playerWins();
+        else if(cpuScore === 5) playerWins();
+    });
+    scissorsButton.addEventListener("click", (event) => {
+        roundResult = playRound(playerSelection = "scissors");
+        console.log(roundResult);
+        if(roundResult === 1) playerScore++;
+        else if(roundResult === -1) cpuScore++;
+        if(playerScore === 5) playerWins();
+        else if(cpuScore === 5) playerWins();
+    });
+}
+
+game()
 //! Adding event listers to the three buttons
 
-const rockButton = document.getElementById("rockButton");
-const paperButton = document.getElementById("paperButton");
-const scissorsButton = document.getElementById("scissorsButton");
-
 //? Just change the function names and make it call the function "play round"
-rockButton.addEventListener("click", (event) => {
-    playRound(playerSelection = "rock");
-});
-paperButton.addEventListener("click", (event) => {
-    playRound(playerSelection = "paper");
-});
-scissorsButton.addEventListener("click", (event) => {
-    playRound(playerSelection = "scissors");
-});
+
+// TODO: INSTEAD OF ROCK PAPER SCISSORS DOOOO POKEMON!!!!! CHARMANDER SQUIRTLE AND BULBASAUR
